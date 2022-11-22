614476827793947a49a117e3f9dc055709df4c7b
abdulquayyum871 <quayyummohd871@gmail.com>
2022-11-22 13:48:09 +0530

    This is for November 22

---
+++ javascript/Operators/Assignment.js
@@ -0,0 +1,21 @@
+// declarinmg a vareiable
+
+var a = 64; //Declaration
+
+console.log(a);
+
+//Tyoe of operator is used to know the type of data type
+console.log(typeof(a));
+
+var a = "Abdul Quayyum"
+
+console.log(a)
+
+console.log(typeof(a))
+
+//2nd
+
+var a,b,c,d=25;
+
+console.log(a,b,c,d,e);
+
