6168a22835da31a7b5f1244871b5e6fba8b18355
abdulquayyum871 <quayyummohd871@gmail.com>
2022-12-13 11:54:18 +0530

    13Dec

--- week3/practice.js
+++ week3/practice.js
@@ -1,3 +1,36 @@
-function fun1(x,y){
+// function fun1(x,y){
+
+// }
+
+var sum = (a, b) => {
+  return a + b;
+  
+};
+console.log(sum(5, 2));
+
+
+var prod = (a, b) => {
+    return a * b;
     
-}
+  };
+  console.log(sum(5, 2));
+
+  
+
+
+  var div = (a, b) => {
+    return a / b;
+    
+  };
+  console.log(div(5, 2));
+
+  
+
+  var mod = (a, b) => {
+    return a % b;
+    
+  };
+  console.log(mod(5, 2));
+  
+
+
