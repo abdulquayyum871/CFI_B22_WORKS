3fc301486440a38b14ad3e614dce7a39381efd6c
abdulquayyum871 <quayyummohd871@gmail.com>
2022-12-15 01:15:41 +0530

    A

---
+++ hoisting/1.js
@@ -0,0 +1,12 @@
+// function fun(n) {
+//     if (n == 0 || n == 1) return n;
+//     if (n % 3 != 0) return 0;
+//     return fun(n / 3);
+// }  
+// console.log(fun(7))
+
+function foo(n, r) {
+    if (n > 0) return n % r + foo(Math.floor(n / r), r);
+    else return 0;
+    }
+    console.log(foo(513, 2));
--- week3/conditional/whileloopp.js
+++ week3/conditional/whileloopp.js
@@ -9,3 +9,21 @@
 //  console.log("afzal")
 
 //  result = (a !== null && a !== undefined) ? a : b;
+
+// function fun(num) {
+//     if (num == 4) {
+//         return num;
+//     }
+//     return 2 * fun(num + 1);
+// }
+
+// var n = 1;
+// console.log(fun(n));
+
+
+function fun(x, y) {
+    if (x == 0) return y;
+    return fun(x - 1, x + y);
+    }
+    fun(4, 3);
+    console.log(fun(4, 3));
