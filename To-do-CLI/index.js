0ac4ca3cb6ee1a12f6ef5236610946bedb006152
abdulquayyum871 <quayyummohd871@gmail.com>
2023-01-03 07:59:34 +0530

    Jan 3

---
+++ To-do-CLI/index.js
@@ -0,0 +1,49 @@
+import readlineSync from "readline-sync"
+import c from "chalk"
+import fs from "fs"
+import loading from "loading-cli"
+function dislaymenu() {
+   
+    console.clear()
+    console.log(c.red("========================================================"))
+    console.log(c.yellowBright.bold.italic("\t \t \t TO-DO CLI"))
+    console.log(c.red("========================================================"))
+    console.log(c.green("\t \t \t MAIN MENU "))
+    console.log(c.red("========================================================"))
+    console.log(c.underline.cyanBright("Choose the Operation you want to Perform"))
+    console.log(c.whiteBright(`
+    1.User Login 
+    2.User Create
+    3.Delete Account
+    4.Exit`))    
+    console.log(c.red("========================================================"))
+    let options = readlineSync.questionInt((">> Enter the No for which operation you want to perform: "))
+    switch(options){
+        case 4:
+            console.log(c.redBright("Thank You for using Our CLI Application"))
+            return
+        case 1:
+            console.log(c.blue("User Login"))
+            break;
+        case 2:
+            console.log(c.blue("User Create"))
+            break;
+        case 3:
+            console.log(c.blue("Delete Account"))
+            break
+        default:
+            console.log("Invalid Options")
+    }
+    let ShouldContine = readlineSync.question("Do you want to continue (y/n)?")
+    if(ShouldContine == "y"||ShouldContine =="Y"||ShouldContine=="yes"||ShouldContine=="Yes"){
+    loading("Redirecting You to MainMenu").start()
+    setTimeout(()=>{
+        dislaymenu()
+      },3000)
+      
+}else{
+    console.clear()
+    return console.log(c.redBright("Thank You for using Our CLI Application"))
+}
+}
+dislaymenu()
---
+++ To-do-CLI/utils/userlogin.js
@@ -0,0 +1,67 @@
+import readlinesync from "readline-sync";
+import fs from "fs";
+import chalk from "chalk";
+
+function createUsers() {
+  let Name = readlinesync.question("Enter Full Name: ");
+  while (!Name) {
+    Name = readlinesync.question("re-Enter Full Name: ");
+  }
+  let username = readlinesync.question("Enter Your user name:");
+  while (!username) {
+    username = readlinesync.question("Enter Your user name: ");
+  }
+  let email = readlinesync.questionEMail("Enter Your Mail: ");
+  while (!email) {
+    email = readlinesync.questionEMail("Enter Your Mail: ");
+  }
+  console.log(
+    chalk.red("Note: The password length should be minimum 8 letters")
+  );
+  let password = readlinesync.question("Enter Your password: ", {
+    hideEchoBack: true,
+  });
+  while (!password || !(password.length >= 8)) {
+    password = readlinesync.question("Enter Your password: ", {
+      hideEchoBack: true,
+    });
+  }
+  let confpassword = readlinesync.question(
+    "Enter Your Password for Confirmation:",
+    { hideEchoBack: true }
+  );
+  while (password != confpassword) {
+    console.log(chalk.red("Password Do Not Match"));
+    confpassword = readlinesync.question(
+      "Enter Your Password for Confirmation",
+      { hideEchoBack: true }
+    );
+  }
+  let filedata = readFile("data.json")
+  filedata = JSON.parse(filedata);
+  console.log(filedata)
+}
+
+createUsers();
+
+let writefile = (filename, filedata) => {
+  return new Promise((resolve, reject) => {
+    fs.write((filename, filedata), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+
+function readFile(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        })
+    })
+}
